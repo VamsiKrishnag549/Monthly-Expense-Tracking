714ba9fd60a9c62d07034d88ba2165b0621788eb
VamsiKrishnag549 <vamsikrishnagokaraju123@gmail.com>
2025-12-21 23:15:10 +0530

    Update UserRepository.java

--- src/main/java/com/expensetracker/repository/UserRepository.java
+++ src/main/java/com/expensetracker/repository/UserRepository.java
@@ -5,5 +5,7 @@ import com.expensetracker.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+	boolean existsByEmail(String email);
 	Optional<User> findByEmail(String email);
 }
+
